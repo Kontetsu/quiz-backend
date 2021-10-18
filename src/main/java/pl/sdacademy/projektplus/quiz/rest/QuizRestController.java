7b0cdfcffb2a253e8710bd2e3c964402cdd3e9fe
Ubuntu <erget.kabaj@gmail.com>
2021-10-18 12:19:25 +0000

    modified by gitlab

--- src/main/java/pl/sdacademy/projektplus/quiz/rest/QuizRestController.java
+++ src/main/java/pl/sdacademy/projektplus/quiz/rest/QuizRestController.java
@@ -6,12 +6,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.sdacademy.projektplus.quiz.dto.CategoriesDto.CategoryDto;
+import pl.sdacademy.projektplus.quiz.dto.GameStateDto;
 import pl.sdacademy.projektplus.quiz.frontend.Difficulty;
 import pl.sdacademy.projektplus.quiz.frontend.GameOptions;
+import pl.sdacademy.projektplus.quiz.frontend.UserAnswer;
 import pl.sdacademy.projektplus.quiz.services.OngoingGameService;
 import pl.sdacademy.projektplus.quiz.services.QuizDataService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/quiz")
@@ -33,8 +36,24 @@ public class QuizRestController {
 
     @PostMapping("/startGame")
     public ResponseEntity<?> startGame(@RequestBody GameOptions gameOptions) {
-        log.info("Starting new game with parameters: " + gameOptions);
-        ongoingGameService.init(gameOptions);
+        final Optional<CategoryDto> category = quizDataService.getQuizCategories().stream().filter(cat -> cat.getId() == gameOptions.getCategoryId()).findFirst();
+        final GameOptions newGameOptions = new GameOptions(gameOptions.getNumberOfQuestions(), gameOptions.getDifficulty(), gameOptions.getCategoryId(), category.orElse(new CategoryDto()).getName());
+        log.info("Starting new game with parameters: " + newGameOptions);
+        ongoingGameService.init(newGameOptions);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+    @GetMapping("/gameState")
+    public GameStateDto getGameState() {
+        return ongoingGameService.getCurrentGameState();
+    }
+
+    @PostMapping("/submitAnswer")
+    public ResponseEntity<?> startGame(@RequestBody UserAnswer userAnswer) {
+        log.info("Received answer: " + userAnswer);
+        ongoingGameService.checkAnswerForCurrentQuestionAndUpdatePoints(userAnswer.getAnswer());
+        ongoingGameService.proceedToNextQuestion();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+    }
+
 }
